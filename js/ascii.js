7c4a2717809e2e24ec8662b3daf6ed4e1d195b04
marcelagiraldo <leidymarcelaalzategiraldo@gmail.com>
2023-01-30 07:03:40 -0500

    traductor listo

--- js/ascii.js
+++ js/ascii.js
@@ -28,9 +28,85 @@ function traer_datos(){
         nuevaFila = '</tr>';
     }
     $("#tbody").html(contendor+nuevaFila); */
-    tabla();
+    tabla(ascii,binario);
 };
 
 function tabla(ascii,binario){
-    
+    var table="<table border=1><th> Código ASCII </th><th>128</th><th>64</th><th>32</th><th>16</th>";
+    table+="<th>8</th><th>4</th><th>2</th><th>1</th> </tr></thead><tbody>";
+	/* table+="<thead><tr>";
+	table+="<tr>";
+	table+="<th> Código ASCII </th>"; */
+	/* table+="<th>128</th>";
+    table+="<th>64</th>";
+    table+="<th>32</th>";
+    table+="<th>16</th>";
+    table+="<th>8</th>";
+    table+="<th>4</th>";
+    table+="<th>2</th>";
+	table+="<th>1</th> </tr></thead>";
+    table+="<tbody>"; */
+    for(j=0;j<binario.length;j++){
+        table+="<tr><td>"+ascii[j]+"</td>";
+        var bin = binario[j];
+        var binary = bin.split("") 
+        console.log(binary)
+        if(binary.length==8){
+            for(k=0;k<8;k++){
+                table+="<td>"+binary[k]+"</td>";
+            }
+        }
+        if(binary.length==7){
+            table+="<td>0</td>"
+            for(k=0;k<7;k++){
+                table+="<td>"+binary[k]+"</td>";
+            }
+        }
+        if(binary.length==6){
+            table+="<td>0</td><td>0</td>"
+            for(k=0;k<6;k++){
+                table+="<td>"+binary[k]+"</td>";
+            }
+        }
+        if(binary.length==5){
+            table+="<td>0</td><td>0</td><td>0</td>"
+            for(k=0;k<5;k++){
+                table+="<td>"+binary[k]+"</td>";
+            }
+        }
+        if(binary.length==4){
+            table+="<td>0</td><td>0</td><td>0</td><td>0</td>"
+            for(k=0;k<4;k++){
+                table+="<td>"+binary[k]+"</td>";
+            }
+        }
+        if(binary.length==3){
+            table+="<td>0</td><td>0</td><td>0</td><td>0</td><td>0</td>"
+            for(k=0;k<3;k++){
+                table+="<td>"+binary[k]+"</td>";
+            }
+        }
+        if(binary.length==2){
+            table+="<td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td>"
+            for(k=0;k<2;k++){
+                table+="<td>"+binary[k]+"</td>";
+            }
+        }
+        if(binary.length==1){
+            table+="<td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td><td>0</td>"
+            for(k=0;k<1;k++){
+                table+="<td>"+binary[k]+"</td>";
+            }
+        }
+        table+="</tr>";
+    }    
+    table+="</tbody>";
+    table+="</table>";
+	document.getElementById("tabla").innerHTML=table;
 }
+
+function refrescar(){
+    document.getElementById("tabla").innerHTML="";
+    document.getElementById("texto").value="";
+};
+
